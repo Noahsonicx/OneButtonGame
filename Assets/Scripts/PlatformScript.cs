1b5b5b0c812713fa6bcaa9c0b534516afe7c38c3
Noahsonicx <53206519+Noahsonicx@users.noreply.github.com>
2021-04-21 17:55:45 +1000

    Prefab Commit
    Created Platform Commit

--- Assets/Scripts/PlatformScript.cs
+++ Assets/Scripts/PlatformScript.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class PlayerController : MonoBehaviour
+public class PlatformScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
